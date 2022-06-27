b6219b9639814a5cd1b156bd72c6ee7bd531813d
ssean567 <ssean567@gmail.com>
2022-06-27 12:37:53 -0400

    added easy_2_2

---
+++ easy_2_2.rb
@@ -0,0 +1,6 @@
+musters_description = "The Munsters are creepy in a good way."
+
+musters_description.swapcase!
+musters_description.capitalize!
+musters_description.downcase!
+musters_description.upcase!
