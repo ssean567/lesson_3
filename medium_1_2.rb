2a41c97aa425e03d5d2a5c5914b198bfb95476bc
ssean567 <ssean567@gmail.com>
2022-06-29 17:14:29 -0400

    added medium_1_2

---
+++ medium_1_2.rb
@@ -0,0 +1,9 @@
+# produces an error. Can not add a string to an integer
+#puts "the value of 40 + 2 is " + (40 + 2)
+
+# my 1st solution using string interpolation
+num = 40 + 2
+puts "the value of 40 + 2 is #{num}"
+
+#converts 42 into a string
+puts "the value of 40 + 2 is " + (40+2).to_s
